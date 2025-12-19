a839711c54a0c81a2a078209edf8291f9a7bd017
LogicMaster <traderrafa02@gmail.com>
2025-12-19 08:50:01 -0300

    inicio sistema cadastro de cliente

---
+++ CADASTRO-CLIENTE.cbl
@@ -0,0 +1,4 @@
+      *****************************************************************
+      * PROGRAMA: CADASTRO-CLIENTE.cbl
+      * DESCRIÇÃO: Sistema completo de cadastro com validações
+      *****************************************************************
